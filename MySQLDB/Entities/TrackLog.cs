617b462407c21b01e1c2270ae556c97de1a9d83f
Gustavo Aguiar <Gustavo.Aguiar@Transcore.com>
2025-10-03 16:24:58 -0300

    track log

---
+++ MySQLDB/Entities/TrackLog.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MySQLDB.Entities
+{
+    [Table("TrackLog")]
+    public class TrackLog : CrudEntity
+    {
+        [Required]
+        [MaxLength(100)]
+        public string SessionId { get; set; }
+
+        [MaxLength(200)]
+        public string? Email { get; set; }
+
+        [MaxLength(100)]
+        public string? Name { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Event { get; set; }
+
+        [MaxLength(250)]
+        public string? Message { get; set; }
+
+        [MaxLength(250)]
+        public string? Path { get; set; }
+
+        [MaxLength(50)]
+        public string? TrafficSource { get; set; }
+
+        [MaxLength(100)]
+        public string? NavigatorUserAgent { get; set; }
+
+        [MaxLength(100)]
+        public string? NavigatorLanguage { get; set; }
+
+        [MaxLength(100)]
+        public string? NavigatorPlatform { get; set; }
+
+        public DateTime? FrontendTimeStamp { get; set; }
+
+        public int? Priority { get; set; }
+    }
+}
---
+++ MySQLDB/Migrations/20250929130808_track.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace MySQLDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class track : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TrackLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    SessionId = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "varchar(200)", maxLength: 200, nullable: true),
+                    Name = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    Event = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false),
+                    Message = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: true),
+                    Path = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: true),
+                    TrafficSource = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    NavigatorUserAgent = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    NavigatorLanguage = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    NavigatorPlatform = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: true),
+                    FrontendTimeStamp = table.Column<DateTime>(type: "DATETIME", nullable: true),
+                    Priority = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "DATETIME", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    UpdatedAt = table.Column<DateTime>(type: "DATETIME", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "DATETIME", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrackLog", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrackLog");
+        }
+    }
+}
